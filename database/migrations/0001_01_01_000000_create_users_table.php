d0e0ce90970f1e47f0b0f437876dee6bfc95cd0b
Davidzen <davidzen662@gmail.com>
2025-11-25 00:00:04 +0800

    Project Laravel Auth

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'seller', 'buyer'])->default('buyer');
-            $table->enum('status', ['pending', 'approved', 'rejected', 'active'])->default('active');
-
             $table->rememberToken();
             $table->timestamps();
         });
